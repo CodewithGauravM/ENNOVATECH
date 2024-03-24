a46c1ed654c5c2a697fe0697e4b5a3d59c955fed
CodewithGauravM <158427895+CodewithGauravM@users.noreply.github.com>
2024-03-24 16:58:20 +0530

    1st time

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// annimation js
+AOS.init();
+
+document.querySelector('.mobile').style.display = "none";
+
+function handleMenu() {
+    document.querySelector('.mobile').style.display = "flex";
+}
+
+function handleClose() {
+    document.querySelector('.mobile').style.display = "none";
+}
+///////////////////
+
+
+(function($){
+	$(document).ready(function(){
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault(); 
+			event.stopPropagation(); 
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+	});
+})(jQuery);
+
+
+ 
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
+  
